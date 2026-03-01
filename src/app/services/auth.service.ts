0b72b4c2e2aef02ea8c03aeb708eec07232c89d6
jer <mjlerba@gmail.com>
2026-03-01 11:41:38 +0200

    updating auth services

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,23 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { Human } from "../models/human";
+import { Router } from "@angular/router";
 
 @Injectable({
     providedIn : 'root'
 })
 export class HumanService {
-    private humans: Human[] = [];
-    constructor(){}
-    addHuman(human: Human){
-        this.humans.push(human);
-    }
+    private router = inject(Router);
+    user = signal<{ id: string; role: 'student' | 'teacher' } | null>(null);
 
-    getHumans(){
-         return this.humans;
+    login(id: string, role: 'student'| 'teacher'){
+      this.user.set({ id, role });
+      this.router.navigate(['/courses']);
+    }
+    logout(){
+      this.user.set(null);
+      this.router.navigate(['/login']);
+    }
+    isAuthenticated(){
+      return this.user() !== null;
     }
 }
