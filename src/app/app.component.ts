0f270a523fb6e8192666514156b33c10563dd438
jer <mjlerba@gmail.com>
2026-03-14 17:13:28 +0200

    clear unuseful code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,4 @@ import {RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '';
-   firstName = signal('monami jerome');
-
-   fistUpper = computed(()=> this.firstName.toString());
-
-  deleteTask(title: string){
-    this.title = "jerome";
-    console.log("task deleted: " + title);
-  }
 }
