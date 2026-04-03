a2eb5ad03ecc17fb5c88730d710ab55de62168d7
jer <mjlerba@gmail.com>
2026-04-03 09:44:12 +0200

    component created

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  protected readonly fb = inject(FormBuilder);
 
+  protected readonly userForm = this.fb.group({
+    username: '',
+    email: '',
+    password: ''
+
+  })
+  protected readonly register(): void{
+    console.log(this.userForm.value)
+  }
 }
