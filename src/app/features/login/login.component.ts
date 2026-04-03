eb6454789878f0f9caf8b993e1ec2e49e185a01c
jer <mjlerba@gmail.com>
2026-04-03 09:27:59 +0200

    add user form

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    private readonly fb = inject(FormBuilder);
 
+    protected readonly userForm = this.fb.group({
+      username: '',
+      email: '',
+      password: ''
+    })
+
+    protected register(): void{
+      console.log(`the users values is ${this.userForm.value}`)
+    }
 }
