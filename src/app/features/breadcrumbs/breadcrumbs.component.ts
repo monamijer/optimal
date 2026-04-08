be25bc53d9cb3e2cf06838ae5a95823f16874179
jer <mjlerba@gmail.com>
2026-04-08 14:31:39 +0200

    fix(breadcrumb): use toSignal + NavigationEnd to update breadcrumb on route change

--- src/app/features/breadcrumbs/breadcrumbs.component.ts
+++ src/app/features/breadcrumbs/breadcrumbs.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+// breadcrumb.component.ts
+import { Component, inject } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BreadcrumbService, Breadcrumb } from '../../core/services/breadcrumb.service';
+import { filter } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-breadcrumbs',
-  imports: [],
+  selector: 'app-breadcrumb',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './breadcrumbs.component.html',
   styleUrl: './breadcrumbs.component.css'
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbComponent {
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  private breadcrumbService = inject(BreadcrumbService);
 
+  // ✅ Se recalcule à chaque NavigationEnd
+  breadcrumbs = toSignal(
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(() => this.breadcrumbService.build(this.activatedRoute.snapshot.root))
+                                              // ✅ .root important !
+    ),
+    { initialValue: [] as Breadcrumb[] }
+  );
 }
