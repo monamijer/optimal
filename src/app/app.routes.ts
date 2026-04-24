8f75a190cadd8831219bba083d438ce13e38fd87
jer <mjlerba@gmail.com>
2026-04-24 16:48:04 +0200

    feat(route) add guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { loggedInGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -13,5 +14,23 @@ export const routes: Routes = [
           loadChildren: ()=> import('./features/courses/courses.routes').then(m => m.COURSES_ROUTES)
         }
       ]
+    },
+    {
+      path: 'login',
+      canActivate: [loggedInGuard],
+      loadComponent: ()=>
+        import('./features/login/login.component').then(m=> m.LoginComponent),
+      data: { breadcrumb: 'Connexion' },
+    },
+    {
+      path: 'register',
+      canActivate: [loggedInGuard],
+      loadComponent: () =>
+        import('./features/register/register.component').then(m=> m.RegisterComponent),
+      data: { breadcrumb: 'Register' }
+    },
+    {
+      path: '**',
+      redirectTo: '',
     }
 ];
