99fe45dad89375b4452e43ded7ae2c3e0ffcb6b1
jer <mjlerba@gmail.com>
2026-04-25 16:17:23 +0200

    feat(guards) add courseAccessGuard and correct route to use parseUrl instead of navigate

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,25 +1,43 @@
 import { inject } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivateFn, Router } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
+import { CourseService } from "../../features/courses/services/course.service";
 
-export const authGuard: CanActivateFn = () =>{
+export const courseAccessGuard: CanActivateFn = (route: ActivatedRouteSnapshot) =>{
   const auth = inject(AuthService);
+  const courseService = inject(CourseService);
   const router = inject(Router);
 
+  const courseId = route.paramMap.get('id');
+  if(!courseId) return router.parseUrl('/courses');
+
+  const course = courseService.getCourseById(courseId);
+
+  if(!course) return router.parseUrl('/courses');
+
+  if(course.accessLevel === 'free') return true;
+
   if(!auth.isLoggedIn()){
-    router.navigate(['/login']);
-    return false;
+    return router.createUrlTree(['/login'], {
+      queryParams: { redirect: `/courses/${courseId}`}
+    })
   }
   return true;
 };
+
+export const authGuard: CanActivateFn = () => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  if(auth.isLoggedIn()) return true;
+  return router.parseUrl('/login');
+}
 
 export const professorGuard: CanActivateFn = ()=>{
   const auth = inject(AuthService);
   const router = inject(Router);
 
   if(auth.isProfessor()) return true;
-  router.navigate(['/']);
-  return false;
+  return router.parseUrl('/');
 }
 
 export const loggedInGuard: CanActivateFn = () => {
@@ -27,6 +45,5 @@ export const loggedInGuard: CanActivateFn = () => {
   const router= inject(Router)
 
   if(!auth.isLoggedIn()) return true;
-  router.navigate(['/']);
-  return false
+  return router.parseUrl('/')
 }
