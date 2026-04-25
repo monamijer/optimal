6597f94d70c6b0f129b755c6b2de65f264a864b7
jer <mjlerba@gmail.com>
2026-04-25 14:12:51 +0200

    feat(course-list) improve design and logic for course-list adding premium and free overview

--- src/app/features/courses/pages/course-list/course-list.component.ts
+++ src/app/features/courses/pages/course-list/course-list.component.ts
@@ -2,20 +2,18 @@ import { ChangeDetectionStrategy, Component, inject, Input, signal } from '@angu
 import { CourseSection } from '../../models/courseSection.model';
 import { CourseSectionComponent } from '../../course-section/course-section.component';
 import { CourseSectionService } from '../../services/course-section.service';
+import { RouterLink } from "@angular/router";
+import { CourseService } from '../../services/course.service';
+import { AuthService } from '../../../../services/auth.service';
 
 @Component({
   selector: 'app-course-list',
-  imports: [CourseSectionComponent],
+  imports: [CourseSectionComponent, RouterLink],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.css',
 })
 export class CourseListComponent {
-  sections = signal([
-    {
-      id: 'algorithms', title: 'Section 1',  content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque.'
-    },
-    {
-    id: 'base_de_donnees', title: 'Section 2',  content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque.'
-     }
-  ])
+  private courseService = inject(CourseService);
+  auth  = inject(AuthService);
+  courses = this.courseService.getAllCourses();
 }
