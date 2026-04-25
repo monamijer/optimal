463d56fb305f2b0e54b7dd12e427b362e4ef2193
jer <mjlerba@gmail.com>
2026-04-25 16:18:41 +0200

    fix(login) move register() to login() in template and add login() in ts

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +10,29 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-    private readonly fb = inject(FormBuilder);
+    private readonly fb     = inject(FormBuilder);
+    private readonly router = inject(Router);
+    private readonly route  = inject(ActivatedRoute)
+    private readonly auth   = inject(AuthService)
+
+    protected readonly error = signal('');
 
     protected readonly userForm = this.fb.group({
-      username: '',
-      email: '',
-      password: ''
+      username: ['', [Validators.required]],
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required],
     })
 
-    protected register(): void{
-      console.log(`the users values is ${this.userForm.value}`)
+    protected login(): void{
+      if(this.userForm.invalid) return;
+      const {email, password } = this.userForm.value;
+      const success = this.auth.login(email!, password!);
+
+      if(success){
+        const redirect = this.route.snapshot.queryParams['redirect'] || '/courses'
+        this.router.navigateByUrl(redirect)
+      }else{
+        this.error.set('Email ou mot de mot de passe incorrect.');
+      }
     }
 }
