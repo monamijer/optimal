27f1dd3c28915e079c3ded425f2b6107c66b365b
jer <mjlerba@gmail.com>
2026-04-25 13:52:32 +0200

    feat(course) updating interface

--- src/app/features/courses/models/course.model.ts
+++ src/app/features/courses/models/course.model.ts
@@ -1,9 +1,14 @@
+import { AccessLevel } from "../../../models/accessLevel";
+
 export interface Course {
     id: string;
     title: string;
-    //description: string;
+    description: string;
+    icon?: string;
+    accessLevel: AccessLevel;
+    tag?: string[];
     //instructor: string;
     ///duration: number; // in hours
     //content: string;
-    icon?: string; // Optional field for course icon
+
 }
